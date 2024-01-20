44fa5e55e4e0d7ed09de32fb3db2a00a1c3829db
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-01-20 11:23:05 +0700

    PHP YOUTUBE

---
+++ lat01.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo " Belajar PHP";
+    echo '<br>';
+
+    echo 'saya siswa SMK';
+
+    echo '<h1 style="background-color:red"> Belajar php itu mudah</h1>
+        <p style="background-color:blue"> saya suka belajar program</p>
+        ';
+
+    echo 'saya belajar' .' PHP'. '<br>';
+
+    echo 2019 + 4;
+
+?>
+
---
+++ lat04.php
@@ -0,0 +1,52 @@
+<?php
+
+    //OPERATOR MATEMATIKA
+    $a = 2;
+    $b = 2;
+
+    $c = $a + $b;
+
+    echo $c. '<br>';
+
+    $c = $a - $b;
+    echo $c. '<br>';
+
+    $c = $a * $b;
+    echo $c. '<br>';
+
+    $c = $a / $b;
+    echo floor($c). '<br>';
+
+    $c = $a % $b;
+    echo $c. '<BR>';
+
+    //OPERATOR LOGIKA
+
+   $c = $a < $b;
+   echo $c;
+
+   $c = $a > $b;
+   echo $c;
+
+   $c = $a == $b;
+   echo $c;
+
+   $c = $a != $b;
+   echo $c.'<br>';
+
+    //INCREMEN
+
+    $a--;
+     echo $a.'<br>';
+
+    //OPERATOR STRING
+
+    $kata = 'Sura';
+    $kota = 'Baya';
+
+    $hasil = $kata.$kota;
+
+    $hasil .=' KOTA PAHLAWAN';
+    echo $hasil;
+
+?>
