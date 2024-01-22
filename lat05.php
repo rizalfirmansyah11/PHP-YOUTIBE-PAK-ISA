537b60ca9dcbe403e9354aeec76e328925861c19
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-01-22 14:37:18 +0700

    Add files via upload

---
+++ lat05.php
@@ -0,0 +1,41 @@
+<?php
+
+    $tanggal = 0;
+
+   // $hasil = $tanggal > 32;
+
+
+    // if ($hasil) {
+    //    if ($tanggal > 0) {
+    //      echo 'Benar';
+    //    }else{
+    //     echo 'salah';
+    //    }
+    // }else {
+    //     echo 'salah';
+    // }
+
+    $nilai = 101;
+
+    // if ($nilai <=100) {
+    //     if ($nilai >= 0) {
+    //         echo 'nilai Benar';
+    //     }else {
+    //         echo 'nilai salah';
+    //     }
+    // }else {
+    //     echo 'Nilai salah';
+    // }
+
+    // if (nilai >=0 && $nilai <= 100 ) {
+    //     echo 'Nilai benar';
+    // }else {
+    //     echo 'Nilai salah';
+    // }
+
+    if ($nilai >= 100 || $nilai <=0    ) {
+        echo 'Nilai benar';
+    }else {
+        echo 'Nilai Benar';
+    }
+?>
